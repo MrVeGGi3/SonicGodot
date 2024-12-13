febb591eb69ee12b67d9f2ca1e27d9c90bbecdb5
Matheus Veras <99488005+MrVeGGi3@users.noreply.github.com>
2024-12-13 14:25:20 -0300

    First Base

---
+++ Scripts/sonic.gd
@@ -0,0 +1,16 @@
+class_name Sonic
+extends RigidBody2D
+
+var velocity : float 
+var max_velocity
+var acceleration_tax : float = 1.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
